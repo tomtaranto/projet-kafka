92c7af60579e93e9a256ec059201d2d1139e1d4a
RagingDeathWish <ciobanita16@gmail.com>
2022-04-14 15:59:26 +0200

    top 10 marchee

--- kafka-stream/src/main/scala/org/esgi/project/api/WebServer.scala
+++ kafka-stream/src/main/scala/org/esgi/project/api/WebServer.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
 import org.apache.kafka.streams.KafkaStreams
-import org.apache.kafka.streams.state.{QueryableStoreTypes, ReadOnlyKeyValueStore, ReadOnlyWindowStore, WindowStoreIterator}
+import org.apache.kafka.streams.state.{QueryableStoreTypes, ReadOnlyKeyValueStore, ReadOnlyWindowStore}
 import org.esgi.project.api.models.{Data, MovieScore, Stats, ViewsCountResponse}
 import org.esgi.project.streaming.StreamProcessing.{meanScoreStoreName, storeMovieID}
 import org.esgi.project.streaming.models.{MeanScoreForMovies, ViewAggregate}
@@ -22,57 +22,51 @@ object WebServer extends PlayJsonSupport {
           val kvStore: ReadOnlyWindowStore[Int, ViewAggregate] = streams.store(storeMovieID, QueryableStoreTypes.windowStore[Int, ViewAggregate]())
           val to: Instant = Instant.now()
           val from_one: Instant = to.minusSeconds(60)
-          val from_five: Instant = to.minusSeconds(5*60)
+          val from_five: Instant = to.minusSeconds(5 * 60)
 
           val keys = kvStore.all().asScala.map(_.key.key()).toList.distinct
 
           complete(
-            if (keys.filter(x=>x==id.toInt).toList.isEmpty)
+            if (keys.filter(x => x == id.toInt).toList.isEmpty)
               List("Movie not found");
             else
               keys
-                .filter(x=> x ==id.toInt)
-                .map(x =>{
-                  val row1min: Iterator[Data] = kvStore.fetch(x,from_one, to).asScala.map((v) =>{
-                    Data(Some(v.value.categorie1_count),Some(v.value.categorie2_count),Some(v.value.categorie3_count))
+                .filter(x => x == id.toInt)
+                .map(x => {
+                  val row1min: Iterator[Data] = kvStore.fetch(x, from_one, to).asScala.map((v) => {
+                    Data(Some(v.value.categorie1_count), Some(v.value.categorie2_count), Some(v.value.categorie3_count))
                   })
                   println(row1min.take(1).toList.headOption)
-                  val row5min: Iterator[Data] = kvStore.fetch(x,from_five, to).asScala.map((v) =>{
-                    Data(Some(v.value.categorie1_count),Some(v.value.categorie2_count),Some(v.value.categorie3_count))
+                  val row5min: Iterator[Data] = kvStore.fetch(x, from_five, to).asScala.map((v) => {
+                    Data(Some(v.value.categorie1_count), Some(v.value.categorie2_count), Some(v.value.categorie3_count))
                   })
-                  val rowAll: Iterator[Data] = kvStore.fetch(x,to.minusSeconds(100000000),to).asScala.map((v) =>{
-                    Data(Some(v.value.categorie1_count),Some(v.value.categorie2_count),Some(v.value.categorie3_count))
+                  val rowAll: Iterator[Data] = kvStore.fetch(x, to.minusSeconds(100000000), to).asScala.map((v) => {
+                    Data(Some(v.value.categorie1_count), Some(v.value.categorie2_count), Some(v.value.categorie3_count))
                   })
 
                   ViewsCountResponse(Some(x),
-                    kvStore.fetch(x,to.minusSeconds(100000000),to).asScala.map(v=>v.value.title).take(1).toList.headOption,
+                    kvStore.fetch(x, to.minusSeconds(100000000), to).asScala.map(v => v.value.title).take(1).toList.headOption,
                     Some(0),
-                      Stats( rowAll.toList.headOption.foldLeft(Data.empty:Data)((a,b)=>a.addData(b.start_only.getOrElse(0f),b.half.getOrElse(0f), b.full.getOrElse(0f))),
-                        row1min.take(1).toList.headOption.getOrElse(Data.empty),
-                        row5min.take(1).toList.headOption.foldLeft(Data.empty:Data)((a,b)=>a.addData(b.start_only.getOrElse(0f),b.half.getOrElse(0f), b.full.getOrElse(0f)))
-                       )
+                    Stats(rowAll.toList.headOption.foldLeft(Data.empty: Data)((a, b) => a.addData(b.start_only.getOrElse(0f), b.half.getOrElse(0f), b.full.getOrElse(0f))),
+                      row1min.take(1).toList.headOption.getOrElse(Data.empty),
+                      row5min.take(1).toList.headOption.foldLeft(Data.empty: Data)((a, b) => a.addData(b.start_only.getOrElse(0f), b.half.getOrElse(0f), b.full.getOrElse(0f)))
                     )
+                  )
                 })
 
           )
         }
       },
-      path("stats" / "ten" /"worst"/"score") {
+      path("stats" / "ten" / "best" / "score") {
         println("Requete ")
         val kvStore: ReadOnlyKeyValueStore[String, MeanScoreForMovies] = streams.store(meanScoreStoreName, QueryableStoreTypes.keyValueStore[String, MeanScoreForMovies]())
-        val availableKeys = kvStore.all().asScala.map(_.key).toList.distinct
 
         get {
           complete(
-            availableKeys
-              .map(
-                x=> {
-                  kvStore.all().asScala.toList
-                    .map((k)=> MovieScore(k.value.title, k.value.meanScore))
 
-                }
-              )
-              .sortBy(_)(implicitly[Ordering[Float]]).reverse
+            kvStore.all().asScala.toList
+              .map((k) => MovieScore(k.value.title, k.value.meanScore))
+              .toList.distinct.sortBy(_.score)(implicitly[Ordering[Float]]).reverse.take(10)
           )
         }
       }
